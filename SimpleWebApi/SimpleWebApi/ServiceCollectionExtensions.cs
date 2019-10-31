fd3e1b6593d4797a8594c49b87c64acca13c8d01
Oleksandr Dudnyk <Oleksandr_Dudnyk@epam.com>
2019-10-31 14:49:10 +0200

    init

---
+++ SimpleWebApi/SimpleWebApi/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleWebApi
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomMetrics(this IServiceCollection services)
+        {
+            AppMetrics.CreateDefaultBuilder()
+                .Configuration
+                .Configure(options =>
+                {
+                    options.Enabled = true;
+                    options.ReportingEnabled = true;
+
+                }).Report.ToConsole(options=>
+                { 
+                    options.FlushInterval= TimeSpan.FromSeconds(5);
+                });
+
+            return services.AddMetrics();
+        }
+    }
+}
---
+++ SimpleWebApi/SimpleWebApi/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Extensions.DependencyInjection;
+using App.Metrics.Reporting.InfluxDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                .Configuration
+                .Configure(options => { })
+                //.Report.ToTextFile(@"D:\metrics.txt", TimeSpan.FromSeconds(5));
+                .Report.ToInfluxDb(
+                    options =>
+                    {
+                        var influxMetricsReportingSettings = Configuration.GetSection("InfluxReporting").Get<InfluxDbOptions>();
+                        influxMetricsReportingSettings.Database = "Database";
+                        options.InfluxDb = influxMetricsReportingSettings;
+                        options.FlushInterval = TimeSpan.FromSeconds(5);
+                    });
+
+            services
+                .AddMetrics(metrics)
+                .AddMetricsReportingHostedService()
+                .AddMetricsTrackingMiddleware();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMetricsAllMiddleware();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
